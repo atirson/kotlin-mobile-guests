74c44fa40783d030a0b9aadf719cb0b98f4edbae
Atirson Fabiano <fabiano.oliveira2213@gmail.com>
2024-08-12 07:31:46 -0300

    config: create and added connection with database

--- app/src/main/java/com/example/guests/repository/GuestRepository.kt
+++ app/src/main/java/com/example/guests/repository/GuestRepository.kt
@@ -1,17 +1,25 @@
 package com.example.guests.repository
 
-class GuestRepository private constructor() {
+import android.content.Context
+
+class GuestRepository private constructor(context: Context) {
+
+    private val guestDataBase = GuestDataBase(context)
 
     // Singleton
     companion object {
         private lateinit var repository: GuestRepository
 
-        fun getInstance(): GuestRepository {
+        fun getInstance(context: Context): GuestRepository {
             if(!Companion::repository.isInitialized) {
-                repository = GuestRepository()
+                repository = GuestRepository(context)
             }
 
             return repository
         }
+    }
+
+    fun save() {
+
     }
 }
--- app/src/main/java/com/example/guests/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/example/guests/viewmodel/GuestFormViewModel.kt
@@ -1,14 +1,15 @@
 package com.example.guests.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.example.guests.repository.GuestRepository
 
-class GuestFormViewModel: ViewModel() {
-    private val respository = GuestRepository
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
+    private val respository = GuestRepository.getInstance(application)
 
     //Example
     fun getUsers() {
-        GuestRepository.getInstance()
     }
 
 }
